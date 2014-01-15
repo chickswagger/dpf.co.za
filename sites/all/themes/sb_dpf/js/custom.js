f39de68e0363d2c7b43c8724156818f4c29cd7c9
nomsa <nomsa@starbright.co.za>
2014-01-15 16:53:12 +0200

    files and folders, updated the css, and added the js file

--- sites/all/themes/sb_dpf/js/custom.js
+++ sites/all/themes/sb_dpf/js/custom.js
@@ -19,5 +19,26 @@ jQuery(document).ready(function($) {
       $(this).parent().children('ul').slideToggle(250);
     });
   }
- 
+ var default_values = new Array();
+  jQuery("input.form-text").css("color", '#444343');
+  
+  jQuery("input.form-text").focus(function() {
+    if (!default_values[this.id]) {
+      default_values[this.id] = this.value;
+	    //this.style.color = '#000';
+	 // jQuery(this).css("color", '#000');
+    }
+    if (this.value == default_values[this.id]) {
+      this.value = '';
+	     this.style.color = '#000000';
+         }
+		 
+    jQuery(this).blur(function() {
+      if (this.value == '') {
+	  this.id=='input.form-text';	
+         this.value = default_values[this.id];
+		 this.style.color = '#444343';
+      }
+    });
+  });
 });
